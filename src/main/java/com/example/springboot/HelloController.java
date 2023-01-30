f58045d8f3c4613bd2c86ccd30c0ee5576109d79
Marshall F <marshall@wecancodeit.org>
2023-01-30 12:37:56 -0500

    Super-simple form submission

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,23 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
+	Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
 	@GetMapping("/hello")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@PostMapping("/process_form")
+	public String process_form(@RequestBody String body) {
+		logger.info(body);
+		return body;
 	}
 }
