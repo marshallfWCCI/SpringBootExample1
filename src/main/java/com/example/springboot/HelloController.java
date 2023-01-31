5c8da1b82df1c8405db1247ee4629bcee2fcb506
Marshall F <marshall@wecancodeit.org>
2023-01-31 13:00:51 -0500

    table

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -12,7 +12,19 @@ public class HelloController {
 
 	@GetMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "<html><body>" +
+				"<table>" +
+				"<tr>" +
+				"<td>1A</td>" +
+				"<td>1B</td>" +
+				"<td>1C</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td>2A</td>" +
+				"<td>2B</td>" +
+				"<td>2C</td>" +
+				"</tr>" +
+				"</table></body></html>";
 	}
 
 	@PostMapping("/process_form")
@@ -20,4 +32,25 @@ public class HelloController {
 		logger.info(body);
 		return body;
 	}
+//	@GetMapping("/api/v1/courses/") public void getAllCourses() {}
+//
+//
+//	@GetMapping("/api/v1/courses/{course_id}/") public void getOneCourse() {}
+//	@GetMapping("/api/v1/courses/{course_id}/modules") public void getAllModulesForOneCourse() {}
+//	@GetMapping("/api/v1/courses/{course_id}/modules/{module_id}") public void getOneModuleForOneCourse() {}
+//	@PostMapping("/api/v1/courses/{course_id}/modules/{module_id}") public void addOneModuleForOneCourse() {}
+//	@PutMapping("/api/v1/courses/{course_id}/modules/{module_id}") public void changeOneModuleForOneCourse() {}
+
+//	@PutMapping("/api/v1/stations/{station_id}/observations/{date}")
+//
+//	@GetMapping("/api/v1/stations/{station_id}/observations/");
+//	@GetMapping("/api/v1/regions/se/best_price");
+
+
+
+
+	// BAD @PutMapping("/api/v1/change_module/{course_id}/{module_id}")
+	// NOT REST (BAD) @PutMapping("/api/v1/change_module/123/456/
+	// REST @PutMapping("/api/v1/courses/123/modules/456") public void changeOneModuleForOneCourse() {}
+
 }
