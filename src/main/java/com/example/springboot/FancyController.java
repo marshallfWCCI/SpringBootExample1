5b373354939030839dea2b5abfd819c7f630cb07
Marshall F <marshall@wecancodeit.org>
2023-02-01 18:35:58 -0500

    Support http://localhost:8080/greeting?name=Marshall

--- src/main/java/com/example/springboot/FancyController.java
+++ src/main/java/com/example/springboot/FancyController.java
@@ -1,12 +1,20 @@
 package com.example.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FancyController {
     @GetMapping("/fancyHello")
     public String fancyHello() {
-        return "fancyHello.html";
+        return "fancyHello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
