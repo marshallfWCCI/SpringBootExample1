c96b140fdbfb5120be4acaa5a9f80b4359c571c7
Marshall F <marshall@wecancodeit.org>
2023-02-06 08:59:18 -0500

    POST REST example

--- src/main/java/com/example/springboot/restControllers/DepartmentRestController.java
+++ src/main/java/com/example/springboot/restControllers/DepartmentRestController.java
@@ -3,11 +3,7 @@ package com.example.springboot.restControllers;
 import com.example.springboot.entities.Department;
 import com.example.springboot.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,13 +15,34 @@ public class DepartmentRestController {
         this.departmentRepository = departmentRepository;
     }
 
+    // curl -v http://localhost:8080/departments
     @GetMapping(value = "/departments")
     public Iterable<Department> departments() {
         return departmentRepository.findAll();
     }
 
+    // curl -v http://localhost:8080/departments/d008
     @GetMapping(value = "/departments/{department_id}")
     public Optional<Department> getDeptById(@PathVariable String department_id) {
         return departmentRepository.findById(department_id);
+    }
+
+    // curl -v http://localhost:8080/departments/d008 -H 'Content-Type: application/json' -d '{"id":"d008","deptName":"Research and Development"}'
+    @PostMapping(value = "/departments/{department_id}")
+    public Department getDeptById(@RequestBody Department newDepartmentEntity,
+                                  @PathVariable String department_id) {
+        final Optional<Department> fromDatabase = departmentRepository.findById(department_id);
+
+        return fromDatabase
+                // What do to if the Optional contains an entity
+                .map((entity) -> {
+                    entity.setDeptName(newDepartmentEntity.getDeptName());
+                    return departmentRepository.save(entity);
+                })
+                // What to do if the Optional doesn't contain an entity.
+                .orElseGet(() -> {
+                    newDepartmentEntity.setId(department_id);
+                    return departmentRepository.save(newDepartmentEntity);
+                });
     }
 }
